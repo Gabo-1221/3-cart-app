fc03791a420211f3d18d61c8e22ff3fa073b7a52
Gabo-1221 <153243524+Gabo-1221@users.noreply.github.com>
2024-08-29 22:05:05 -0600

    cambios de curso

--- src/app/components/cart-app.component.ts
+++ src/app/components/cart-app.component.ts
@@ -4,11 +4,12 @@ import { Product } from '../models/product';
 import { CatalogComponent } from './catalog/catalog.component';
 import { CartComponent } from './cart/cart.component';
 import { CartItem } from '../models/cartItem';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'cart-app',
   standalone: true,
-  imports: [CatalogComponent, CartComponent],
+  imports: [CatalogComponent, CartComponent, NavbarComponent],
   templateUrl: './cart-app.component.html'
 })
 export class CartAppComponent implements OnInit {
@@ -19,11 +20,13 @@ export class CartAppComponent implements OnInit {
 
   total: number = 0;
 
+  showCart: boolean= false;
+
   constructor(private service:ProductService){ }
 
   ngOnInit(): void {
     this.products = this.service.findAll();
-    this.items = JSON.parse(sessionStorage.getItem('cart')!) || [];
+    this.items = JSON.parse(sessionStorage.getItem('cart') || '[]') ;
     this.calcularTotal();
   }
 
@@ -61,5 +64,9 @@ export class CartAppComponent implements OnInit {
 
   saveSession(): void {
     sessionStorage.setItem('cart', JSON.stringify(this.items))
+  }
+
+  openCart():void{
+    this.showCart = !this.showCart;
   }
 }
